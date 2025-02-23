b32f42c4216ff5b93a7ef575b8a2610ed3045853
eladdagan <eladdagan5@gmail.com>
2025-02-23 15:30:30 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const users = {
-    "אלעד": "2323",
-    "ינאי": "2222",
-    "נועה": "4545",
-    "סיון": "0987"
+    "אלעד": { password: "2323", rank: "ראש צוות", progress: 100 },
+    "ינאי": { password: "2222", rank: "ראש צוות", progress: 100 },
+    "נועה": { password: "4545", rank: "מנהל", progress: 50 },
+    "סיון": { password: "0987", rank: "פקיד", progress: 25 }
 };
 
 document.getElementById('login-form').addEventListener('submit', function(event) {
@@ -10,10 +10,12 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (users[username] && users[username] === password) {
+    if (users[username] && users[username].password === password) {
         document.querySelector('.login-container').style.display = 'none';
         document.querySelector('.logout-container').style.display = 'block';
         document.getElementById('welcome-message').innerText = `שלום ${username}, מה תרצה לעשות היום?`;
+        document.getElementById('rank-message').innerText = `דרגה: ${users[username].rank}`;
+        document.getElementById('progress').style.width = `${users[username].progress}%`;
     } else {
         document.getElementById('message').innerText = 'שם משתמש או סיסמה שגויים';
     }
@@ -30,7 +32,7 @@ document.getElementById('change-password-button').addEventListener('click', func
     const newPassword = prompt('הכנס סיסמה חדשה:');
     if (newPassword) {
         const username = document.getElementById('welcome-message').innerText.split(' ')[1];
-        users[username] = newPassword;
+        users[username].password = newPassword;
         alert('הסיסמה שונתה בהצלחה');
     }
 });
